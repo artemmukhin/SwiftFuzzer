7429c03eb6aa46768004f9f7b0e0d479b8501323
ortem <ortem00@gmail.com>
2018-03-23 21:22:34 +0300

    Initial Commit

---
+++ SwiftFuzzer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftFuzzer
+//
+//  Created by Artem Mukhin on 23.03.2018.
+//  Copyright © 2018 Artem Mukhin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
